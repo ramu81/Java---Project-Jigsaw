49310fa4bc6fb8d8b84876e0731b3ba0d7b4bc3f
Ramu <ramudu.kp@gmail.com>
2017-10-24 22:15:33 -0400

    Add files via upload

---
+++ src/edu.greetings/edu/chinna/kadira/Greetings.java
@@ -0,0 +1,6 @@
+ package edu.chinna.kadira;
+    public class Greetings {
+        public static void main(String[] args) {
+            System.out.println(" Hello Greetings!");
+        }
+    }
---
+++ src/edu.greetings/module-info.java
@@ -0,0 +1,1 @@
+ module edu.greetings { }
